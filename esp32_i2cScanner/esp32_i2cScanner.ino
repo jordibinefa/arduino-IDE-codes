85774cfd7d16181e101cc2ee8d63d7d6936226b5
jordibinefa <40146788+jordibinefa@users.noreply.github.com>
2018-06-11 10:22:33 +0200

    Add files via upload

---
+++ esp32_i2cScanner/esp32_i2cScanner.ino
@@ -0,0 +1,50 @@
+// https://playground.arduino.cc/Main/I2cScanner?action=sourceblock&num=1
+
+#include <Wire.h>
+
+#define I2C_SDA 21
+#define I2C_SCL 22
+
+void setup() {
+  Wire.begin(I2C_SDA, I2C_SCL);
+  Serial.begin(115200);
+  Serial.println("\nI2C Scanner");
+}
+
+void loop() {
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for (address = 1; address < 127; address++ )  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.print(address, HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error == 4)    {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.println(address, HEX);
+    }
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+  delay(5000);           // wait 5 seconds for next scan
+}
