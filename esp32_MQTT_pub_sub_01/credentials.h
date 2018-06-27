56184f8dd622df95f0e97a906158a59cf3608b74
jordibinefa <40146788+jordibinefa@users.noreply.github.com>
2018-06-27 20:33:48 +0200

    Add files via upload

--- esp32_MQTT_pub_sub_01/credentials.h
+++ esp32_MQTT_pub_sub_01/credentials.h
@@ -1,8 +1,10 @@
 //const char* ssid     = "yourSSID";
 //const char* password = "yourPassword";
 
-const char* ssid     = "IoT-eCat";
-const char* password = "clotClot";
+//const char* ssid     = "IoT-eCat";
+//const char* password = "clotClot";
+const char* ssid     = "Robotica2018";
+const char* password = "1123prim2018";
 
 const char* mqtt_server = "popotamo.binefa.cat";
 const int mqtt_port = 1888; // normally 1883
