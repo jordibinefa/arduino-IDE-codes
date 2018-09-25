adc9fb5109b38734ffdf95903d2c0b412857ba13
jordibinefa <40146788+jordibinefa@users.noreply.github.com>
2018-09-25 10:53:51 +0200

    Add files via upload

---
+++ esp32_tinyRTC_forceTimeAndData/esp32_tinyRTC_forceTimeAndData.ino
@@ -0,0 +1,107 @@
+#include <Wire.h>
+#define ESP32
+#include "RTClib.h"
+DS1307 rtc;
+
+#define I2C_SDA 21
+#define I2C_SCL 22
+
+#define I2C_SDA_OLED 4
+#define I2C_SCL_OLED 15
+
+void vShowTime() {
+  DateTime now = rtc.now();
+
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(' ');
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+
+  Serial.print(" since midnight 1/1/1970 = ");
+  Serial.print(now.unixtime());
+  Serial.print("s = ");
+  Serial.print(now.unixtime() / 86400L);
+  Serial.println("d");
+
+  // calculate a date which is 7 days and 30 seconds into the future
+  DateTime future (now.unixtime() + 7 * 86400L + 30);
+
+  Serial.print(" now + 7d + 30s: ");
+  Serial.print(future.year(), DEC);
+  Serial.print('/');
+  Serial.print(future.month(), DEC);
+  Serial.print('/');
+  Serial.print(future.day(), DEC);
+  Serial.print(' ');
+  Serial.print(future.hour(), DEC);
+  Serial.print(':');
+  Serial.print(future.minute(), DEC);
+  Serial.print(':');
+  Serial.print(future.second(), DEC);
+  Serial.println();
+
+  Serial.println();
+}
+
+void setup() {
+  Wire.begin(I2C_SDA, I2C_SCL);
+  //Wire.begin(I2C_SDA_OLED, I2C_SCL_OLED);
+  Serial.begin(115200);
+  Serial.println("\nForcing time and data from your computer");
+  rtc.begin();
+
+  //if (! rtc.isrunning()) {
+    //Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(__DATE__, __TIME__));
+  //}
+}
+
+void loop() {
+  static int nCmpt = 1;
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for (address = 1; address < 127; address++ )  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.print(address, HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error == 4)    {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.println(address, HEX);
+    }
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+  vShowTime();
+
+  delay(5000);           // wait 5 seconds for next scan
+}
